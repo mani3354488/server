cef1392a1f1223c9a326ba7924b5762a0cdec528
mani3354488 <mani3354488@gmail.com>
2020-09-22 13:53:18 +0530

    setting graphql playground

--- src/index.js
+++ src/index.js
@@ -2,20 +2,39 @@
 
 import express from 'express';
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 import bodyParser from 'body-parser';
+import { createServer } from 'http';
+import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 
-import './config/db'
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
 
 app.use(bodyParser.json());
 
-app.listen(PORT, err => {
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH
+}))
+
+app.use(constants.GRAPHQL_PATH, graphqlExpress({
+    schema
+}))
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(constants.PORT, err => {
     if(err) {
         console.error(err);
     } else {
-        console.log(`App listen to port: ${PORT}`);
+        console.log(`App listen to port: ${constants.PORT}`);
     }
 });
  
